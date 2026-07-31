feaa2e70464584178469af8c4608b471cb1ae5ce
huangtank <tank.huang@outlook.com>
2026-07-31 19:24:51 +0800

    fix: 修正 q03_dance 第二關 example

--- examples/quests/q03_dance/state_2/main.py
+++ examples/quests/q03_dance/state_2/main.py
@@ -2,23 +2,28 @@ from minethon import create_bot
 
 bot = create_bot("g_dance")
 queue = []
+flag = True 
 
 while True:
     if bot.get_block(39, 63, -403) == "glass":
         if bot.get_block_property(40, 66, -406, "lit") and flag:
             queue.append(1)
+            flag = False
         elif bot.get_block_property(38, 66, -406, "lit") and flag:
             queue.append(2)
+            flag = False
         elif bot.get_block_property(36, 66, -406, "lit") and flag:
             queue.append(3)
+            flag = False
         elif bot.get_block_property(42, 66, -406, "lit") and flag:
             queue.append(4)
+            flag = False
             
-        while (bot.get_block_property(40, 66, -406, "lit") != 0 or 
-               bot.get_block_property(38, 66, -406, "lit") != 0 or 
-               bot.get_block_property(36, 66, -406, "lit") != 0 or 
-               bot.get_block_property(42, 66, -406, "lit") != 0):
-            continue
+        elif (bot.get_block_property(40, 66, -406, "lit") == 0 and 
+               bot.get_block_property(38, 66, -406, "lit") == 0 and 
+               bot.get_block_property(36, 66, -406, "lit") == 0 and 
+               bot.get_block_property(42, 66, -406, "lit") == 0):
+            flag = True
 
     else:
         for step in queue:    
